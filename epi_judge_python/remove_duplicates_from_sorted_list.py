f3b5c282e7b04a4881fdbd398756bdf977e3e86f
Michael J Chen <bleriotusa@gmail.com>
2020-07-11 23:09:06 -0700

    Add sorted array remove duplicates

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -6,7 +6,26 @@ from test_framework import generic_test
 
 def remove_duplicates(L: ListNode) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return None
+    open_spot = None
+    i1, i2 = L, L.next
+    while i2:
+        if i2.data == i1.data:
+            if not open_spot:
+                open_spot = i2
+        elif i1 != i2:
+            if open_spot:
+                open_spot.data = i2.data
+                open_spot = open_spot.next
+        i1, i2 = i2, i2.next
+
+    valid_elements = 1
+    current = L
+    while current.data != open_spot.data:
+        valid_elements += 1
+        print(current.data)
+        current = current.next
+
+    return L
 
 
 if __name__ == '__main__':
--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -7,8 +7,26 @@ from test_framework.test_utils import enable_executor_hook
 
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if len(A) == 1:
+        return 1
+    elif not A:
+        return 0
+
+    open_spot = -1
+    i1, i2 = 0, 1
+    while i2 < len(A):
+        if A[i2] == A[i1]:
+            if open_spot == -1:
+                open_spot = i2
+        elif A[i1] != A[i2]:
+            if open_spot != -1:
+                A[open_spot] = A[i2]
+                open_spot += 1
+
+        i1 += 1
+        i2 += 1
+    print(A)
+    return open_spot if open_spot != -1 else len(A)
 
 
 @enable_executor_hook
